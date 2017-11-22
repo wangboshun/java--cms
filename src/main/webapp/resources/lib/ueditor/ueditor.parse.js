c417760cb2a7ec6211124d2609df3f28249a67ab
wbs <512742341@qq.com>
2017-11-22 15:05:03 +0800

    update

--- src/main/webapp/resources/lib/ueditor/ueditor.parse.js
+++ src/main/webapp/resources/lib/ueditor/ueditor.parse.js
@@ -641,7 +641,7 @@ UE.parse.register('charts',function( utils ){
         if ( !window.jQuery ) {
 
             utils.loadFile(document,{
-                src : resourceRoot + "/third-party/jquery-1.10.2.min.js",
+                src :"/resources/lib/jquery/jquery.min.js",
                 tag : "script",
                 type : "text/javascript",
                 defer : "defer"
